d973e0cdf89da7fac0f0f617b67e70730ec26a86
Natallia_Gorbunova <natallia_gorbunova@epam.com>
2019-01-07 14:37:52 +0100

    exclude 2-way data binding; use template variable

--- src/app/course-list/search/search.component.ts
+++ src/app/course-list/search/search.component.ts
@@ -1,25 +1,16 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
-  searchValue: string  = '';
+export class SearchComponent {
   @Output() setSearchPhrase: EventEmitter<string> = new EventEmitter<string>();
   constructor() {}
 
-  onClick() {
-    console.log(this.searchValue);
-    this.setSearchPhrase.emit(this.searchValue);
+  updateSearchValue(value: string) {
+    console.log(value);
+    this.setSearchPhrase.emit(value);
   }
-
-  checkIfEmpty() {
-    if (!this.searchValue) {
-      this.setSearchPhrase.emit('')
-    }
-  }
-
-  ngOnInit() {}
 }
