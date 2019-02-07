44f3898bfff7e13aba95d42bfb0a156e5242cbe5
Natallia_Gorbunova <natallia_gorbunova@epam.com>
2019-02-07 13:53:59 +0100

    redirect to courses after login

--- src/app/login/login-page/login-page.component.ts
+++ src/app/login/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,12 +10,14 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginPageComponent implements OnInit {
   login: string = '';
   password: string = '';
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
   authenticate() {
-    this.authService.login({ login: this.login, password: this.password });
+    if(this.authService.login({ login: this.login, password: this.password })) {
+      this.router.navigate(['/courses']);
+    }
     console.log('logged in successfully');
   }
 }
