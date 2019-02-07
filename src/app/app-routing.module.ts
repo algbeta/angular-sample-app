c053d0356d001e6844410f037caa7f626462ef6a
Natallia_Gorbunova <natallia_gorbunova@epam.com>
2019-02-07 13:36:20 +0100

    page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,20 @@ import { CoursesComponent } from './course-list/courses/courses.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
 import { CourseFormComponent } from './course-list/course-form/course-form.component';
 import { EditCourseFormComponent } from './course-list/edit-course-form/edit-course-form.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { CoreModule } from './core/core.module';
 
 const routes: Routes = [
   { path: '', component: CoursesComponent },
   { path: 'courses', component: CoursesComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'course-edit', component: CourseFormComponent },
-  { path: 'course-edit/:id', component: EditCourseFormComponent }
+  { path: 'course-edit/:id', component: EditCourseFormComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), CourseListModule],
+  imports: [RouterModule.forRoot(routes), CourseListModule, CoreModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,18 +4,21 @@ import { HeaderComponent } from './header/header.component'
 import { FooterComponent } from './footer/footer.component'
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component'
 import { SharedModule } from '../shared/shared.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PageNotFoundComponent
   ],
   imports: [CommonModule, SharedModule],
   exports: [
     HeaderComponent,
     FooterComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PageNotFoundComponent
   ]
 })
 export class CoreModule {}
